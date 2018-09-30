adb43f261a73b32376a316915b05129961d1f926
Florian ARNOULD <florian.arnould@ecole.ensicaen.fr>
2018-09-30 14:38:37 +0200

    add creating table 'users' in the prepare script

--- prepare.js
+++ prepare.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const sq = require('better-sqlite3');
 
 let secret = crypto.randomBytes(20).toString('hex');
 let content = "module.exports = {\n" +
@@ -10,3 +11,12 @@ fs.writeFile('config.js', content, function (err) {
   if (err) throw err;
   console.log('Saved!');
 });
+
+let db = new sq('database.db3');
+let sql = 'CREATE TABLE IF NOT EXISTS users (\n' +
+            '\tid INTEGER PRIMARY KEY AUTOINCREMENT,\n' +
+            '\tlogin TEXT NOT NULL,\n' +
+            '\tpassword TEXT NOT NULL,\n' +
+            '\ttoken TEXT);';
+db.exec(sql);
+db.close();
